db47af1b820a32dfa240444133d61894e8f0dcc5
Christian Dittrich <christian.dittrich@creatale.de>
2024-01-27 17:22:53 +0100

    integrated build mode with level selection

--- levels/level_script.gd
+++ levels/level_script.gd
@@ -1,9 +1,13 @@
-extends Node
+extends Node3D
 
+var spawnController: SpawnController
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Gamestate.change_state.connect(_on_state_change)
+	spawnController = load("res://ui/SpawnController.tscn").instantiate()
+	spawnController.level = self
+	add_child(spawnController)
 
 func _on_state_change(new_state: Gamestate.StateChange):
 	match new_state:
