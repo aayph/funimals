fe628a78fff50f2220c7450651864bddd8430853
Mara | Aayph <mmehlitz@gmail.com>
2024-01-27 08:43:30 +0100

    adding jolt physics

--- scripts/meeple.gd
+++ scripts/meeple.gd
@@ -1,4 +1,4 @@
-extends Node
+extends CharacterBody3D
 class_name Meeple
 
 var happiness: float
@@ -7,3 +7,10 @@ func change_happiness(happiness_change: float) -> float:
 	happiness += happiness_change
 	happiness = clamp(happiness, -1.0, 1.0)
 	return happiness
+
+func _ready() -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	velocity.x = 1.0
+	move_and_slide()
