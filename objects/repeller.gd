cf116fbee866dfb6272f09dcd44fc5c685f07d42
Mara | Aayph <mmehlitz@gmail.com>
2024-01-27 13:18:38 +0100

    adding parameters to repeller and attractors

--- objects/repeller.gd
+++ objects/repeller.gd
@@ -1,5 +1,17 @@
 extends Node3D
 class_name Repeller
 
+@export var base_strength: float = 1.0
+@export var happiness_influence: float = 0.0
+@export var max_distance: float = 20.0
+@export var distance_influence: float = 0.0
+
 func get_repell_strength(position: Vector3, happiness: float = 0.0) -> float:
-	return 1.0
+	var strength := base_strength
+	strength += happiness_influence * happiness
+	var distance = transform.origin.distance_to(position)
+	if max_distance >= 0.0:
+		if distance <= max_distance:
+			return 0.0
+		strength += (distance / max_distance) * distance_influence
+	return strength
